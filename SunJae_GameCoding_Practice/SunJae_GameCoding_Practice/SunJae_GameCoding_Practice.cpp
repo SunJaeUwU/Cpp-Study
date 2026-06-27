a93477e793fcd5aae608e530e6fee49be16483b1
SunJaeUwU <790321aaa@gmail.com>
2026-06-27 19:26:10 +0900

    데이터와 로직
    프로그래밍 개론
    변수, 정수, 산술 연산, 비교 연산, 분기문

---
+++ SunJae_GameCoding_Practice/SunJae_GameCoding_Practice/SunJae_GameCoding_Practice.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+// [타입] [이름];
+
+// char   1
+// short  2
+// int    4
+// long long   8
+
+// 문자 char , string
+
+// ==
+// !=
+// <
+// <=
+// >
+// >=
+
+// ! not
+// && and
+// || or
+
+// if ( ) { }
+// if - else
+// if - else if - else
+
+int hp;
+
+int main()
+{
+	hp = 100;
+
+	if (hp <= 0)
+	{
+		// 플레이어 사망
+		cout << "죽었습니다" << endl;
+		cout << "살았습니다" << endl;
+	}
+}
