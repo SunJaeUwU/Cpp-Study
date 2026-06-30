5aebd86b1f6315e11b41bbd4de80358efd76262e
SunJaeUwU <790321aaa@gmail.com>
2026-06-30 21:45:14 +0900

    함수 연습

---
+++ SunJae_GameCoding_Practice/Week_02_Test/Week_02_Test.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+
+// [타입] [이름];
+
+// [리턴타입] [함수이름]()
+int TestFuctionAdd(int a, float b)
+{
+    cout << "Hello World Test  " << a + b <<  endl;
+
+    int sum = (a + b);
+
+    return sum;
+}
+
+
+int main()
+{
+
+    TestFuctionAdd(10, 3.14f);
+
+}
